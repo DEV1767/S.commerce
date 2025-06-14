1bde9d281f5f1ccc622f42c55e169b127a81cb0b
SHIVAM CHAUDHARY <Shivamchy076@gmail.com>
2025-06-14 23:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const cources=document.getElementById("d2")
+cources.addEventListener("click",()=>{
+       window.open("cources.html", "_blank");
+
+})
+const apply=document.getElementById("apply")
+apply.addEventListener("click",()=>{
+       window.open("https://docs.google.com/forms/d/e/1FAIpQLSdEk2UiJwaMqsVl8My9JNwtUUAVfx81M0sOCPQGkRqFbpDpmQ/viewform?usp=dialog","_blank");
+})
